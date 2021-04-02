a46c8d35be1f459624d7f7edf7f6ec12899254c2
Your Name <you@example.com>
2021-04-02 11:59:48 +0000

    update5

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ Route::get("livre/enregistrer",      [MainController::class, "enregistrerLivre"]
 
 Route::get('livre/liste',            [MainController::class, 'getList'])->name('livre.liste');
 
-Route::get('afficher/livre/{livre}', [MainController::class, 'editLivre'])->name('ph');
+Route::get('afficher/livre/{livre}', [MainController::class, 'editLivre'])->name('livre.afficher');
 
 Route::post('modifier/{id}',         [MainController::class, 'updateLivre'])->name('livre.modifier');
 
